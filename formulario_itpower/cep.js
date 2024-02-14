78cb69e0b51cd1e59f1a7179f9c69bf139db8e8c
MariCarriel <104836440+MariCarriel@users.noreply.github.com>
2024-02-14 02:44:00 -0300

    Add files via upload

---
+++ formulario_itpower/cep.js
@@ -0,0 +1,21 @@
+$("#cep").blur(function () {
+    var cep = this.value.replace(/[^0-9]/, "");
+
+    if (cep.length != 8) {
+        return false;
+    }
+
+
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+
+
+    $.getJSON(url, function (dadosRetorno) {
+        try {
+
+            $("#rua").val(dadosRetorno.logradouro);
+            $("#bairro").val(dadosRetorno.bairro);
+            $("#uf").val(dadosRetorno.uf);
+        } catch (ex) { }
+    });
+});
